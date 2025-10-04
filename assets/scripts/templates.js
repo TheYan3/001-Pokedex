be43e2ce270c193a5156f52df44125d7afa1e5ca
TheYan3 <yannic-jundt@gmx.de>
2025-10-05 00:56:32 +0200

    refactor(layout): update templates and CSS for improved structure and consistency
    Refines the overall page layout by adjusting HTML templates and corresponding CSS.
    Enhances visual hierarchy, spacing, and maintainability without changing functionality.

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -1,11 +1,14 @@
 function pokemonCardTemplate({ name, id, imgUrl, type }) {
     return `
       <article class="pokemon-card type-${type}" data-id="${id}" data-name="${name}">
-        <span class="pokemon-id">#${String(id).padStart(3, "0")}</span>
-        <div>
-          <h2>${name.charAt(0).toUpperCase() + name.slice(1)}</h2>
-        </div>
-        <img src="${imgUrl}" alt="${name}" />
+      <div class="pokemon-overflow">
+      <img class="img-type" src="./assets/img/types/${type}-img.svg" alt="${type}" />
+      <div class="pokemon-info">
+      <h2>${name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+      <span class="pokemon-id">#${String(id).padStart(3, "0")}</span>
+      </div>
+      </div>
+      <img src="${imgUrl}" alt="${name}" />
       </article>
     `;
   }
@@ -13,3 +16,9 @@ function pokemonCardTemplate({ name, id, imgUrl, type }) {
   function errorTemplate() {
     return `<p>⚠️ Fehler beim Laden der Pokémon!</p>`;
   }
+
+  function isLoadingTemplate() {
+    return `Lade Pokémon...`;
+  }
+  
+  
