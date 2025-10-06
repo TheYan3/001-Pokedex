ac5f61598ab5b00c1e79969bebfa360a208f92f2
TheYan3 <yannic-jundt@gmx.de>
2025-10-06 23:24:41 +0200

    feat(styles): add hover effects (blur and zoom) and improve responsiveness

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -1,13 +1,13 @@
 function pokemonCardTemplate({ name, id, imgUrl, type }) {
     return `
-      <article class="pokemon-card type-${type}" data-id="${id}" data-name="${name}">
+      <article onclick="openInfo()" class="pokemon-card type-${type}" data-id="${id}" data-name="${name}">
         <div class="type-bg">
           <img class="img-type" src="./assets/img/types/${type}-img.svg" alt="${type}" />
         </div>
         <img class="pokemon-img" src="${imgUrl}" alt="${name}" />
         <div class="pokemon-info">
-          <h2>${name.charAt(0).toUpperCase() + name.slice(1)}</h2>
           <span class="pokemon-id">#${String(id).padStart(3, "0")}</span>
+          <h2>${name.charAt(0).toUpperCase() + name.slice(1)}</h2>
         </div>
       </article>
     `;
